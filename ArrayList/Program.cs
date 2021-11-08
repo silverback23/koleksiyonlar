23de3c47d365d283ccde521255cd34acf082b040
silverback23 <cahid23@hotmail.com>
2021-11-08 15:41:41 +0300

    first commit

---
+++ ArrayList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //system.Collections namesapace
+
+            ArrayList liste= new ArrayList();
+            liste.Add("ayşe");
+            liste.Add(2);
+
+            //içerisindeki elamanlara erişim
+
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<T> class     
+            //System.Collections.Generic
+            // T -> object türündendir
+
+            List<int> sayiListesi = new List<int>();
+            sayiListesi.Add(12);
+            sayiListesi.Add(172);
+            sayiListesi.Add(89);
+
+            List<string> RenkListesi = new List<string>();
+            RenkListesi.Add("Kırmızı");
+            RenkListesi.Add("Mavi");
+            RenkListesi.Add("Yeşil");
+            //Diziyi listeye çevirme
+            string [] hayvanlar = {"kedi","kuş","balık"};
+            List<string> hayvanlistesi= new List<string>(hayvanlar);
+            
+
+           
+        }
+    }
+}
